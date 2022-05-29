a5ba976863e49e425bb371e8475fa3627c6365d6
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-05-29 10:27:08 +1000

    Plan.md

--- src/test/java/DiplomaAutoTest.java
+++ src/test/java/DiplomaAutoTest.java
@@ -1,26 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import static com.codeborne.selenide.Selenide.open;
 
 public class DiplomaAutoTest {
-
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
-        try {
-            Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/?user=root&password=9mREsvXDs9Gk89Ef");
-            Statement s = (Statement) conn.createStatement();
-            int result = s.executeUpdate("CREATE DATABASE mysql");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     @Test
     void shouldOpen() {
