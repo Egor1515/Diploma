d544d525767a501f050519d1297a59d53999e2c4
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-06-22 22:05:57 +1000

    Db

--- src/test/java/dataBase/Main.java
+++ src/test/java/dataBase/Main.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DbConnections {
-    public static void mysql() throws ClassNotFoundException, SQLException {
+public class Main {
+    public static void main(String[] args)  throws ClassNotFoundException, SQLException {
         String url = "jdbc:mysql://localhost:3306/app";
         String userName = "app";
         String password = "9mREsvXDs9Gk89Ef";
