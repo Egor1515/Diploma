ddd88650d4001c75d9e01260acb05baaa992cb41
Egor Bondar <egorbondar@MacBook-Air-Egor.local>
2022-06-22 21:09:31 +1000

    dbConnectors

--- src/test/java/dataBase/DbConnections.java
+++ src/test/java/dataBase/DbConnections.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnections {
-    public static void mysql() throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         String url = "jdbc:mysql://localhost:3306/app";
         String userName = "app";
         String password = "9mREsvXDs9Gk89Ef";
@@ -13,16 +13,15 @@ public class DbConnections {
         try (Connection connection = DriverManager.getConnection(url, userName, password)) {
             System.out.println("We're connected");
         }
-
     }
 
-//    public static void postgresql() throws ClassNotFoundException, SQLException {
-//        String url = "";
-//        String userName = null;
-//        String password = null;
-//        Class.forName("com.postgresql.jdbc.Driver");
-//        try (Connection connection = DriverManager.getConnection(url, userName, password)) {
-//            System.out.println("We're connected");
-//        }
-//    }
+        public static void postgresql() throws ClassNotFoundException, SQLException {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String userName = "root";
+            String password = "root";
+            Class.forName("com.postgresql.jdbc.Driver");
+            try (Connection connection = DriverManager.getConnection(url, userName, password)) {
+                System.out.println("We're connected");
+            }
+        }
 }
